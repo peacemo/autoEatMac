374d194e41b621dab8fe2a8fc26f8b74d28473c5
peacemo <1321500713@qq.com>
2021-03-05 17:51:50 +0800

    improve UI

--- app/src/main/java/com/carlchenxq/zidong/MainActivity.java
+++ app/src/main/java/com/carlchenxq/zidong/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
